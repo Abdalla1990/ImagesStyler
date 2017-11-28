cc5e176d5de6a7bae3ab51275721ce7c3fdae86f
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-11-28 16:11:02 -0500

    config

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-const path = require('path');
-const OutPutPath = path.join(__dirname, './public/');
-app.use(express.static(OutPutPath));
-
-app.listen(process.env.PORT || 8080);
